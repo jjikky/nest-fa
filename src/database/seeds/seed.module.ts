a2c7ca2236a57466c139ddb513603208c9a717a1
jjikky <kok5417@skuniv.ac.kr>
2024-10-22 16:41:04 +0900

    feat: seed 모듈 추가

---
+++ src/database/seeds/seed.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { UserSeedModule } from './user/user-seed.module';
+import databaseConfig from '../config/database.config';
+import appConfig from 'src/config/app.config';
+import { TypeOrmConfigService } from '../typeorm-config.service';
+
+@Module({
+  imports: [
+    UserSeedModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig, appConfig],
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+      dataSourceFactory: async (options: DataSourceOptions) => {
+        return new DataSource(options).initialize();
+      },
+    }),
+  ],
+})
+export class SeedModule {}
