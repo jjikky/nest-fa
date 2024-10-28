9d091acf97393eaf87ec0ec37466b513425d547e
jjikky <kok5417@skuniv.ac.kr>
2024-10-29 00:26:10 +0900

    chore: 게시글 수정 dto 변경

--- src/board/dto/update-board.dto.ts
+++ src/board/dto/update-board.dto.ts
@@ -1,14 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional, MaxLength, MinLength } from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
 
 export class UpdateBoardDto {
-  @IsOptional()
-  @MinLength(2)
-  @MaxLength(20)
-  @ApiProperty({ description: '제목', required: true, example: '지키' })
-  title?: string;
-
-  @IsOptional()
+  @IsNotEmpty()
   @ApiProperty({ description: '내용', required: true, example: 'hi' })
   contents?: string;
 }
