ed332864c3abde0fa4086b96b838257e0a28c490
jjikky <kok5417@skuniv.ac.kr>
2024-10-29 18:05:48 +0900

    feat: 유저 생성 기능 추가

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, ValidationPipe } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  signup() {}
+  @Post()
+  signup(@Body(new ValidationPipe()) data: CreateUserDto) {
+    return this.userService.createUser(data);
+  }
 
   login() {}
 
@@ -13,10 +17,6 @@ export class UserController {
 
   @Get()
   async getUsers() {
-    try {
-      return await this.userService.getUser();
-    } catch (error) {
-      console.log(error);
-    }
+    return this.userService.getUser();
   }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -3,6 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './infrastructure/db/entities/user.entity';
 import { Repository } from 'typeorm';
 import { BoardEntity } from '../board/infrastructure/db/entities/board.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -10,6 +12,16 @@ export class UserService {
     @InjectRepository(UserEntity)
     private userRepository: Repository<UserEntity>,
   ) {}
+
+  async createUser(data: CreateUserDto) {
+    const { username, name, password } = data;
+    const encryptedPassword = await this.encryptPassword(password);
+    return this.userRepository.save({
+      username,
+      name,
+      password: encryptedPassword,
+    });
+  }
 
   async getUser() {
     const qb = this.userRepository.createQueryBuilder('User');
@@ -21,5 +33,10 @@ export class UserService {
         .where('Board.userId = User.id');
     }, 'User_boardCount');
     return qb.getMany();
+  }
+
+  async encryptPassword(password: string) {
+    const DEFAULT_SALT = 11;
+    return await hash(password, DEFAULT_SALT);
   }
 }
