0163d2dd84a9374ec6fbc7796314248d565b774e
jjikky <kok5417@skuniv.ac.kr>
2024-10-31 13:18:01 +0900

    feat: 사용자 인증 서비스 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/modules/user/user.service';
+import { compare } from 'bcrypt';
+import { UserEntity } from 'src/modules/user/infrastructure/db/entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async validateUser(username: string, password: string) {
+    const user = await this.userService.getUserByUsername(username);
+    if (user) {
+      const match = await compare(password, user.password);
+      if (match) {
+        return user;
+      }
+      return null;
+    }
+    return null;
+  }
+
+  async login(user: UserEntity) {
+    const payload = { username: user.username, name: user.name };
+    return {
+      accessToken: await this.jwtService.signAsync(payload, {
+        secret: 'secret',
+        expiresIn: '1h',
+      }),
+    };
+  }
+}
