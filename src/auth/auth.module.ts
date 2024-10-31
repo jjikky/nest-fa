897a57c5ffa7e51da5d1b6c4cac105acd37ad6f5
jjikky <kok5417@skuniv.ac.kr>
2024-10-31 13:17:45 +0900

    chore: JWT 인증 모듈 설정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from 'src/modules/user/infrastructure/db/entities/user.entity';
+import { UserModule } from '../modules/user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './auth.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
-  providers: [AuthService],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: { expiresIn: '1h' },
+    }),
+    TypeOrmModule.forFeature([UserEntity]),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
