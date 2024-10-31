b011dfc572be11a626f570837274302921fdeb7c
jjikky <kok5417@skuniv.ac.kr>
2024-10-31 13:18:36 +0900

    feat: 인증 엔드포인트 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get, Ip, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Ip,
+  Post,
+  Query,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private readonly configService: ConfigService,
+    private readonly authService: AuthService,
   ) {}
 
   @Get()
@@ -19,5 +31,17 @@ export class AppController {
   @Get('name')
   getName(@Query('name') name: string): string {
     return `${name}`;
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -17,8 +17,6 @@ export class UserController {
     return this.userService.login(data);
   }
 
-  me() {}
-
   @Get()
   async getUsers() {
     return this.userService.getUser();
