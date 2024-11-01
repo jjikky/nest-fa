5b8498b36d0b4e377f39e61017f9878e8cde6fa8
jjikky <kok5417@skuniv.ac.kr>
2024-11-01 16:55:03 +0900

    refactor: 게시판 서비스 유저 엔티티 의존성 제거

--- src/modules/board/board.service.ts
+++ src/modules/board/board.service.ts
@@ -3,14 +3,11 @@ import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { UserEntity } from 'src/modules/user/infrastructure/db/entities/user.entity';
 import { BoardEntity } from './infrastructure/db/entities/board.entity';
 
 @Injectable()
 export class BoardService {
   constructor(
-    @InjectRepository(UserEntity)
-    private userRepository: Repository<UserEntity>,
     @InjectRepository(BoardEntity)
     private boardRepository: Repository<BoardEntity>,
   ) {}
@@ -32,11 +29,6 @@ export class BoardService {
   }
 
   async create(data: CreateBoardDto) {
-    const user = await this.userRepository.findOne({
-      where: { id: data.userId },
-    });
-    if (!user) throw new Error('User not found');
-
     const board = this.boardRepository.create({
       ...data,
     });
