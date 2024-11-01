8124a0433261c2af01125ea5e6007ba3b4e59379
jjikky <kok5417@skuniv.ac.kr>
2024-11-01 16:55:22 +0900

    feat: 게시판 서비스 테스트 케이스 추가

--- src/modules/board/board.service.spec.ts
+++ src/modules/board/board.service.spec.ts
@@ -1,18 +1,70 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BoardService } from './board.service';
+import { Repository } from 'typeorm';
+import { BoardEntity } from './infrastructure/db/entities/board.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('BoardService', () => {
-  let service: BoardService;
+  let boardService: BoardService;
+  let boardRepository: Repository<BoardEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BoardService],
+      providers: [
+        BoardService,
+        {
+          provide: getRepositoryToken(BoardEntity),
+          useValue: {
+            create: jest.fn(),
+            save: jest.fn(),
+            find: jest.fn(),
+            findOne: jest.fn(),
+            findOneBy: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<BoardService>(BoardService);
+    boardService = module.get<BoardService>(BoardService);
+    boardRepository = module.get<Repository<BoardEntity>>(
+      getRepositoryToken(BoardEntity),
+    );
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('boardService should be defined', () => {
+    expect(boardService).toBeDefined();
+  });
+
+  it('boardRepository should be defined', () => {
+    expect(boardRepository).toBeDefined();
+  });
+
+  //게시글 전체 조회
+  // describe('게시글 전체 조회', () => {
+  //   it('게시글 전체 조회', async () => {
+  //     const result = await boardService.findAll();
+  //     expect(result).toEqual([]);
+  //   });
+  // });
+
+  describe('게시글 조회', () => {
+    it('2번 게시글 작성자는 jikky다', async () => {
+      jest.spyOn(boardRepository, 'findOne').mockResolvedValue({
+        id: 1,
+        userId: 2,
+        contents: 'test',
+        user: {
+          id: 2,
+          username: 'jikky',
+          password: '1234',
+          boards: [],
+          boardCount: 0,
+        },
+      } as BoardEntity);
+      const result = await boardService.findOne(2);
+      expect(result.user.username).toEqual('jikky');
+    });
   });
 });
